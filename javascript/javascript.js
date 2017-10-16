e7f0176be61c6aa682bab0338419ed7d2bfc9b81
Jim Weisman <jweisman@students.mchenry.edu>
2017-10-16 09:57:29 -0500

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,35 @@
+
+function change01(){
+ document.getElementById("large").src="Nola01.jpg";
+}
+
+function change02(){
+    document.getElementById("large").src="Nola02.jpg";
+}
+function change03(){
+    document.getElementById("large").src="Nola17.jpg";
+}
+
+function change04(){
+    document.getElementById("large").src="Nola04.jpg";
+}
+
+function change05(){
+    document.getElementById("large").src="Nola05.jpg";
+}
+
+function change06(){
+    document.getElementById("large").src="Nola03.jpg";
+}
+function change07(){
+    document.getElementById("large").src="Nola24.jpg";
+}
+function change08(){
+    document.getElementById("large").src="Nola32.jpg";
+}
+function change09(){
+    document.getElementById("large").src="Nola11.jpg";
+}
+function change10(){
+    document.getElementById("large").src="Nola12.jpg";
+}
